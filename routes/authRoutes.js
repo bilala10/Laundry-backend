7bf5becf73d239bd79ed57cdf2e83e44f569da1f
bilala10 <bilalahmed2000@hotmail.com>
2024-05-24 06:33:34 +0000

    Fixed bugs

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,8 @@
 // routes/authRoutes.js
 const express = require('express');
-const { registerUser, loginUser, getUserProfile } = require('../controllers/authController');
+const { registerUser } = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/register', registerUser);
-// Uncomment the following lines once the corresponding controller functions are implemented
-// router.post('/login', loginUser);
-// router.get('/profile', protect, getUserProfile);
 
 module.exports = router;
